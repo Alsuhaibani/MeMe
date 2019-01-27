772b36bf7db7438bcf57633e34f5d0ab74c02b77
Alsuhaibani <Alsuhaibani@Alsuhaibanis-MacBook-Pro.local>
2019-01-27 14:55:20 +0300

    2nd commit

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Alsuhaibani on 20/11/2018.
+//  Copyright © 2018 Alsuhaibani. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topTextField: String
+    var bottomTextField: String
+    var originalImage: UIImageView
+    var memedImage: UIImage
+}
